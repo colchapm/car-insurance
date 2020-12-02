dfe48afbe190ba1c4d925aef04d262c8cce9e6c4
Collin Chapman <cchap14@gmail.com>
2020-12-01 18:17:10 -0800

    create initial html file, give car insurance quote based on gender and age.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,21 @@
-$(document).ready(function() {
-  const age = parseInt(prompt("How old are you?"));
+$(document).ready(function(){
+  $("form#insurance").submit(function(event) {
+    const age = parseInt($("input#age").val());
+    const gender = $("select#gender").val();
 
-  if (age >= 21) {
-    $('#drinks').show();
-  } else {
-    $('#under-21').show();
-  }
+    if (age) {
+      let quote = (100 - age) * 3;
+      if (gender === 'male' && age < 26) {
+        quote += 50;
+      }
+  
+      $("#rate").text(quote);
+      $("#quote").show();
+    } else {
+      alert('Please enter your age.');
+    }
+
+
+    event.preventDefault();
+    });
 });
